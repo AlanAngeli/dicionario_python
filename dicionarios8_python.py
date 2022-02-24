0e309c94b79c53c2c1fccdc8134d3f50c5b56214
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-23 22:32:39 -0300

    Add files via upload

---
+++ dicionarios8_python.py
@@ -0,0 +1,42 @@
+perguntas = {
+    "Pergunta 1" : {
+        "Pergunta": "Quanto é 2+2?",
+        "respostas": {"a":"1", "b":"4", "c":"5"},
+        "resposta_certa:": "b",
+
+    },
+    "Pergunta 2": {
+        "Pergunta": "Quanto é 3*2?",
+        "respostas": {"a": "4", "b": "10", "c": "6"},
+        "resposta_certa:": "c",
+
+    },
+}
+print()
+
+respostas_certas = 0
+for pk, pv in perguntas.items():
+    print(f'{pk}:{pv["Pergunta"]}')
+    print("Respostas")
+
+    for rk, rv in pv["respostas"].items():
+        print(f'[{rk}]:{rv}')
+
+
+    print()
+    resposta_usuario = input("Sua resposta: ")
+    print()
+
+    if resposta_usuario == pv["resposta_certa:"]:
+        print("Você acertou!")
+        respostas_certas +=1
+    else:
+        print("Você errou!")
+
+qtd_perguntas = len(perguntas)
+porcentagem_acerto = respostas_certas / qtd_perguntas * 100
+
+print(f'Você acertou {respostas_certas} respostas')
+print(f'Sua porcentagem de acerto foi {porcentagem_acerto}%')
+        
+        
